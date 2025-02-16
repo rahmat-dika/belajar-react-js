0418dd013f65eba15b592b1fc417d5a961c45a58
rahmat-dika <rahmat.andyka.pratama@gmail.com>
2025-02-17 00:12:14 +0700

    hello world

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -13,11 +13,42 @@
 // Penggunaan XML mewajibkan setiap element di tutup
 
 // Format 2
+// export default function HelloWorld() {
+//   return (
+//     <>
+//       <h1>Hello World</h1>
+//       <p>Selamat Belajar React JS Bro</p>
+//     </>
+//   )
+// }
+
+// satu file component bisa mengembalikan lebih dari 1 component
 export default function HelloWorld() {
+  const textP = 'halo cak wes madang urung?'; //mengirim konstanta untuk di tampilkan di jsx
   return (
-    <>
-      <h1>Hello World</h1>
-      <p>Selamat Belajar React JS Bro</p>
-    </>
+    <div>
+      <HeaderHelloWorld/>
+      <ParagraphHelloWorld/>
+      <p>{textP.toLowerCase()}</p> 
+    </div>
+  )
+}
+
+function HeaderHelloWorld(){
+  return (
+    <h1 style={{  //menambahkan style pada js di dalam element langsung. kurung kurawal 1 untuk element dan kurung kurawal 2 untuk object
+      color: "red",
+      backgroundColor: "aqua"
+     }}>Hello World</h1>
+  )
+}
+
+function ParagraphHelloWorld(){
+  const style = { //menambahkan style menggunakan konstanta js
+    color: "yellow",
+    backgroundColor: "red"
+  };
+  return (
+    <p style={style}>Sel amat Belajar React JS Bro</p>
   )
 }
